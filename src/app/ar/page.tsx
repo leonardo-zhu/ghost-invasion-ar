7841cd6c0e82d8aab843623da68a3184ae080a88
Leonardo Zhu <leonardo-zhu@qq.com>
2025-04-27 21:35:22 +0100

    :fire: remove StepProvider Context

--- src/app/ar/page.tsx
+++ src/app/ar/page.tsx
@@ -4,7 +4,6 @@ import ARCanvas from './ARCanvas';
 import Step0 from './Step0';
 import Step1 from './Step1';
 import Step2 from './Step2';
-import StepProvider, {useStep} from '@/components/Step';
 import {useMindAR} from '@/hooks/useMindAR';
 import {setupTerminal, setupWhiteboard} from '@/scenes';
 import {START_MARKER_ID, STEP1_MARKER_ID, STEP2_MARKER_ID} from '@/constants';
@@ -12,7 +11,8 @@ import {START_MARKER_ID, STEP1_MARKER_ID, STEP2_MARKER_ID} from '@/constants';
 export default function ARPage() {
 	const containerRef = useRef<HTMLDivElement>(null);
 
-	const {loading, registerAnchor, markerStates} = useMindAR(containerRef);
+	const {loading, registerAnchor, markerStates, modelMap} =
+		useMindAR(containerRef);
 
 	useEffect(() => {
 		setupWhiteboard(registerAnchor);
@@ -25,30 +25,31 @@ export default function ARPage() {
 		return id;
 	}, [markerStates]);
 
+	const Step = () => {
+		if (currentStep !== undefined) {
+			const model = modelMap.get(currentStep)!;
+
+			switch (currentStep) {
+				case START_MARKER_ID:
+					return <Step0 model={model} />;
+				case STEP1_MARKER_ID:
+					return <Step1 model={model} />;
+				case STEP2_MARKER_ID:
+					return <Step2 model={model} />;
+				default:
+					return null;
+			}
+		}
+		return null;
+	};
+
 	return (
 		<>
 			<ARCanvas containerRef={containerRef} loading={loading} />
 			<div className="absolute top-10 left-1/2 -translate-x-1/2 text-white bg-black/60 px-4 py-2 rounded-xl z-[1010]">
 				Please use landscape mode on your phone
 			</div>
-			<StepProvider externalStep={currentStep}>
-				<Step />
-			</StepProvider>
+			<Step />
 		</>
 	);
 }
-
-const Step = () => {
-	const {step} = useStep();
-
-	switch (step) {
-		case START_MARKER_ID:
-			return <Step0 />;
-		case STEP1_MARKER_ID:
-			return <Step1 />;
-		case STEP2_MARKER_ID:
-			return <Step2 />;
-		default:
-			return null;
-	}
-};
