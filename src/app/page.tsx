18f9407086653cfc8760f5990efa2ebfa74c0918
Leonardo Zhu <leonardo-zhu@qq.com>
2025-04-27 21:34:10 +0100

    :bug: fix href link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,8 +32,6 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/ar"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             <Image
               className="dark:invert"
@@ -47,8 +45,6 @@ export default function Home() {
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
             href="/docs"
-            target="_blank"
-            rel="noopener noreferrer"
           >
             Read docs
           </a>
