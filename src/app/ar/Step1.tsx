9cbfa50fee008a7b6dea0720b42a16eda65f8655
Leonardo Zhu <leonardo-zhu@qq.com>
2025-04-27 21:35:41 +0100

    :art: enhance Step1 with typewriter effect for dialogue and conditional button rendering

--- src/app/ar/Step1.tsx
+++ src/app/ar/Step1.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import * as THREE from 'three';
+import useTypeWriter from '@/hooks/useTypeWriter';
 
 interface Props {
 	model: THREE.Group;
 }
 
+const title =
+	'Do you think the data should be retained forever, or deleted when the task is completed?';
+
 const Step1: React.FC<Props> = ({model: ghostModel}) => {
+	const displayedTitle = useTypeWriter(title);
+
 	const onClickRetain = () => {
 		ghostModel.traverse((obj: any) => {
 			if (obj.isMesh && obj.material) {
@@ -37,25 +43,24 @@ const Step1: React.FC<Props> = ({model: ghostModel}) => {
 
 	return (
 		<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-black/60 px-4 py-2 rounded-xl z-[1010]">
-			<h1 className="text-3xl font-bold">Ghost：</h1>
-			<p className="text-xl">
-				Do you think the data should be retained forever, or deleted when the
-				task is completed?
-			</p>
-			<div className="flex gap-4 mt-4">
-				<button
-					onClick={onClickRetain}
-					className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-				>
-					Retain
-				</button>
-				<button
-					onClick={onClickDelete}
-					className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-				>
-					Delete
-				</button>
-			</div>
+			<h1 className="text-3xl font-bold">👻 Ghost：</h1>
+			<p className="text-xl">{displayedTitle}</p>
+			{displayedTitle === title && (
+				<div className="flex gap-4 mt-4">
+					<button
+						onClick={onClickRetain}
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+					>
+						Retain
+					</button>
+					<button
+						onClick={onClickDelete}
+						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+					>
+						Delete
+					</button>
+				</div>
+			)}
 		</div>
 	);
 };
