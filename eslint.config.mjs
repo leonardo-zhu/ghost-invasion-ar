731ac17974c20076f759f61c58215bd00358deda
Leonardo Zhu <leonardo-zhu@qq.com>
2025-04-27 21:34:58 +0100

    :wrench: update prettier & eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,22 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import prettierPlugin from "eslint-plugin-prettier";
-import prettierConfig from "eslint-config-prettier";
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import {FlatCompat} from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+		rules: {
+			// Add your custom rules here
+			'@typescript-eslint/no-unused-vars': 'warn',
+		},
+	}),
 ];
-
-
-eslintConfig.push(
-  {
-    plugins: {
-      prettier: prettierPlugin,
-    },
-    rules: {
-      "prettier/prettier": "warn",
-    }
-  },
-  ...compat.extends(prettierConfig)
-);
 
 export default eslintConfig;
