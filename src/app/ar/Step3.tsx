98c658de5dd7807927c3b16b2ca928696cc667a3
Leonardo Zhu <leonardo-zhu@qq.com>
2025-04-28 18:07:33 +0100

    :zap: enhance Step3 component with loading state and success message for memory upload

--- src/app/ar/Step3.tsx
+++ src/app/ar/Step3.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Input} from 'antd';
+import {Alert, Input, Space, Spin} from 'antd';
 import {UNIQUE_CODE} from '@/constants';
 
 const {OTP} = Input;
@@ -8,11 +8,25 @@ interface Props {
 	model: THREE.Group | undefined;
 }
 
-const Step3: React.FC<Props> = () => {
+const Step3: React.FC<Props> = ({model}) => {
 	const [isSuccess, setIsSuccess] = useState(false);
+	const [loading, setLoading] = useState(false);
+
 	const onChange = (value: string) => {
-		if (value === UNIQUE_CODE) {
-			setIsSuccess(true);
+		if (value.length === 8) {
+			setLoading(true);
+
+			new Promise<boolean>((resolve) => {
+				setTimeout(() => {
+					if (value === UNIQUE_CODE.replace(/–/g, '')) {
+						resolve(true);
+					}
+					setLoading(false);
+				}, 2000);
+			}).then((value) => {
+				setIsSuccess(value);
+				if (model) model.visible = false;
+			});
 		}
 	};
 
@@ -28,21 +42,33 @@ const Step3: React.FC<Props> = () => {
 			</div>
 		);
 	}
+
 	return (
 		<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white bg-black/60 px-4 py-2 rounded-xl z-[1010]">
 			<h1 className="text-3xl font-bold">👻 Ghost：</h1>
-			<p className="text-xl animate-pulse font-bold">
-				Do you want to upload my memory to the cloud and restore my reputation?
-				Or... permanently shut down this system?
-			</p>
-			<div className="flex justify-center items-center">
-				<OTP
-					onChange={onChange}
-					length={8}
-					formatter={(str) => str.toUpperCase()}
-					separator={(i) => <span>{i === 3 ? '–' : ''}</span>}
-				/>
-			</div>
+			<Spin spinning={loading}>
+				<Space direction="vertical">
+					<Alert
+						type="info"
+						message="Do you want to upload my memory to the cloud and restore my
+								reputation? Or... permanently shut down this system?"
+						description={
+							<p className="text-xl animate-pulse font-bold">
+								Please enter the correct 8-digit code to upload my memory.
+							</p>
+						}
+					/>
+
+					<div className="flex justify-center items-center">
+						<OTP
+							onChange={onChange}
+							length={8}
+							formatter={(str) => str.toUpperCase()}
+							separator={(i) => <span>{i === 3 ? '–' : ''}</span>}
+						/>
+					</div>
+				</Space>
+			</Spin>
 		</div>
 	);
 };
